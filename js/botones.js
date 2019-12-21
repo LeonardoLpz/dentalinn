5577f21c9b6757c4a4780a86be407da8e440e245
Leonardo Lopez <leodanlors1993@gmail.com>
2019-12-21 13:03:56 -0600

    Empieza la migracion a react

---
+++ js/botones.js
@@ -0,0 +1,24 @@
+function whatsapp(){
+    console.log('whatsapp')
+    /* location.href = ' https://wa.me/525534410219' */
+    return gtag_report_conversion('https://wa.me/525534410219')
+}
+function instagram(){
+    console.log('instagram')
+    location.href = ' https://www.instagram.com/dentalinn.san.angel/'
+}
+function facebook(){
+    console.log('facebook')
+    location.href = ' https://www.facebook.com/Dental-Inn-537351753451918/'
+}
+function youtube(){
+    console.log('yuotube')
+    location.href = ' https://www.youtube.com/channel/UCKg6xNuKWaY-eI6bj1qihNA'
+}
+function llamar(){
+    console.log('llamar')
+    location.href = ' tel:+5215534410219'
+}
+
+
+
